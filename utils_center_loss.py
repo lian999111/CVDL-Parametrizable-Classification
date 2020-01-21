6d6f22febe14f5db20ad67eafd698a00e481bb8d
Larry Liang <lian999111@gmail.com>
2020-01-21 01:59:16 +0100

    Add test_one_step_centerloss

--- utils_center_loss.py
+++ utils_center_loss.py
@@ -50,6 +50,19 @@ def train_one_step_centerloss(model, additional_layer,
 
     return softmax_loss, center_loss, total_loss, centers
 
+@tf.function
+def test_one_step_centerloss(model, additional_layer,
+                x_batch_test, y_batch_test, 
+                scce_fn, center_loss_fn, ratio, metric):
+    encodings = model(x_batch_test)
+    logits = additional_layer(encodings)
+    softmax_loss = scce_fn(y_batch_test, logits)
+    center_loss, centers = center_loss_fn(y_batch_test, encodings)
+    total_loss = softmax_loss + tf.math.scalar_mul(ratio, center_loss)
+    metric.update_state(y_batch_test, tf.argmax(logits, 1))
+    
+    return softmax_loss, center_loss, total_loss
+
 def train_model_with_centerloss(model, train_data, train_labels,
                 test_data, test_labels, num_classes, len_encoding,
                 num_epochs=20, batch_size=128,
@@ -64,39 +77,40 @@ def train_model_with_centerloss(model, train_data, train_labels,
     # Additional layer for softmax loss (cross-entropy loss)
     additional_layer = tf.keras.layers.Dense(num_classes)
 
-    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
-    metric = tf.metrics.Accuracy()
-
     # Get loss function objects
     scce_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
     center_loss_fn = CenterLoss(alpha, num_classes, len_encoding)
 
-    # Placeholder for total loss over an epoch
-    overall_total_loss = tf.Variable(0, dtype=tf.float32)
-    
+    # Create metric obj for averaging total loss over an epoch
+    train_loss = tf.keras.metrics.Mean('train_loss', dtype=tf.float32)
+    train_metric = tf.metrics.Accuracy('train_accuracy', dtype=tf.float32)
+    test_loss = tf.keras.metrics.Mean('test_loss', dtype=tf.float32)
+    test_metric = tf.metrics.Accuracy('test_accuracy', dtype=tf.float32)
+
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+
     # Train network
     for epoch in range(num_epochs):
         # Iterate over minibatches
-        metric.reset_states()
-        overall_total_loss.assign(0.0)
+        train_loss.reset_states()
+        train_metric.reset_states()
         for step, (x_batch_train, y_batch_train) in enumerate(train_dataset):
             softmax_loss, center_loss, total_loss, centers = train_one_step_centerloss(model, additional_layer, 
                                                                                        x_batch_train, y_batch_train, 
                                                                                        scce_fn, center_loss_fn, ratio, 
-                                                                                       optimizer, metric)
+                                                                                       optimizer, train_metric)
         
-            overall_total_loss.assign_add(softmax_loss + center_loss)
-        train_accuracy = metric.result()
-        overall_total_loss.assign(overall_total_loss / num_train_samples)
-        print('Epoch: {}: Train Loss: {}, Train Accuracy: {}'.format(epoch, overall_total_loss.numpy(), train_accuracy))
+            train_loss(total_loss)
+        print('Epoch: {}: Train Loss: {}, Train Accuracy: {}'.format(epoch, train_loss.result(), train_metric.result()))
 
         # Model evaluation on test set for this epoch
-        metric.reset_states()
         test_dataset = tf.data.Dataset.from_tensor_slices((test_data, test_labels))
         test_dataset = test_dataset.batch(batch_size)
+        test_loss.reset_states()
+        test_metric.reset_states()
+
         for x_batch_test, y_batch_test in test_dataset:
-            encodings_test = model(x_batch_test)
-            logits = additional_layer(encodings_test)
-            metric.update_state(y_batch_test, tf.argmax(logits, 1))
-        test_accuracy = metric.result()
-        print('Epoch: {}: Test Accuracy: {}'.format(epoch, test_accuracy))
+            softmax_loss, center_loss, total_loss = test_one_step_centerloss(model, additional_layer, x_batch_test, y_batch_test, 
+                                                                             scce_fn, center_loss_fn, ratio, test_metric)
+            test_loss(total_loss)
+        print('Epoch: {}: Test Accuracy: {}'.format(epoch, test_metric.result()))
