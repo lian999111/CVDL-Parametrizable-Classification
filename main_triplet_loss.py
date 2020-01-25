e06cd41357b97eebd58b6bed2e144d73e64d241f
Estrada <humberto.estrada@tu-dortmund.de>
2020-01-25 18:08:46 +0100

    updated main triplet loss

--- main_triplet_loss.py
+++ main_triplet_loss.py
@@ -1,8 +1,20 @@
 # %%
-import tensorflow as tf 
+# TODO: This makes sure the weights are initialized the same, but results after training still not reproducible
+seed_value = 1
+import os
+os.environ['PYTHONHASHSEED']=str(seed_value)
+os.environ['TF_CUDNN_DETERMINISTIC'] = '1'  # new flag present in tf 2.0+
+import random
+random.seed(seed_value)
 import numpy as np 
+np.random.seed(seed_value)
+import tensorflow as tf 
+tf.random.set_seed(seed_value)   
+
 import DLCVDatasets
-import lenet_model
+import models
+from utils import cal_pairwise_dists, l2_normalize
+import matplotlib.pyplot as plt
 import train_triplet_loss
 
 # %% Load and preprocess data
@@ -16,9 +28,9 @@ x_train, y_train, x_test, y_test, class_names = DLCVDatasets.get_dataset(dataset
                                                                          training_size=train_size,
                                                                          test_size=test_size)
 # Normalization
-mean = np.mean(x_train)
-x_train -= mean
-x_test -= mean
+#mean = np.mean(x_train)
+#x_train -= mean
+#x_test -= mean
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
 # Reshape to add the channel dimension
@@ -27,15 +39,22 @@ x_test = np.reshape(x_test, x_test.shape+(1,))
 input_shape = x_train.shape[1:]
 
 # %% Get the model
-model = lenet_model.get_lenet_model(input_shape=input_shape)
+encoding_dim = 64
+normalized_encodings = True
+model = models.get_model_v1(input_shape, encoding_dim, normalized_encodings)
 model.summary()
 
-# %% Train the model with triplet loss
+# %% Train the model with triplet lossnum_epochs = 20
+num_epochs=10
+batch_size = 64
+learning_rate = 0.0001
+margin=0.5
+triplet_loss_strategy="batch_hard"
 train_triplet_loss.train_model_with_tripletloss(model, x_train, y_train,
-                                              x_test, y_test, num_classes=10, len_encoding=64,
-                                              num_epochs=10, batch_size=64, learning_rate=0.0001,
-                                              margin=0.5,  triplet_loss_strategy="batch_hard")
-
+                                              x_test, y_test, num_classes, encoding_dim,
+                                              num_epochs, batch_size, learning_rate,
+                                              margin,  triplet_loss_strategy)
+#model.save('model_tl_lenet_1.h5')
 
 # %% Evaluate the model
 # Load the complete dataset, including 0 - 9
@@ -48,21 +67,88 @@ x_train, y_train, x_test, y_test, class_names = DLCVDatasets.get_dataset(dataset
 x_train = np.reshape(x_train, x_train.shape+(1,))
 x_test = np.reshape(x_test, x_test.shape+(1,))
 
-x_2 = x_test[y_test == 2]
-encoding_2_0 = tf.math.l2_normalize(model(x_2[[0],]))   # this strange [[0],] indexing is to keep dimensions after index slicing
-encoding_2_1 = tf.math.l2_normalize(model(x_2[[20],]))
+# Sort test samples in ascending order (from 0 to 9)
+sorted_idc = np.argsort(y_test, kind='stable')
+x_test = x_test[sorted_idc]
+y_test = y_test[sorted_idc]
 
-x_5 = x_test[y_test == 5]
-encoding_5_0 = tf.math.l2_normalize(model(x_5[[0],]))
-encoding_5_1 = tf.math.l2_normalize(model(x_5[[20],]))
+# Normalized sorted test data for prediction
+x_test_normalized = x_test / 255.0
 
-x_6 = x_test[y_test == 6]
-encoding_6_0 = tf.math.l2_normalize(model(x_6[[0],]))
-encoding_6_1 = tf.math.l2_normalize(model(x_6[[20],]))
+# Store the indices for each digit in dict
+digits_idc = {}
+for idx in range(10):
+    digits_idc[str(idx)] = np.argwhere(y_test == idx)
 
-x_9 = x_test[y_test == 9]
-encoding_9_0 = tf.math.l2_normalize(model(x_9[[0],]))
-encoding_9_1 = tf.math.l2_normalize(model(x_9[[20],]))
+# Compute encodings and pairwise euclidean distances
+encodings = model.predict(x_test_normalized)
+normalized_encodings = l2_normalize(encodings)
+pairwise_dists = cal_pairwise_dists(normalized_encodings)
+
+# %%
+img2_0_idx = digits_idc['2'][0]
+img2_1_idx = digits_idc['2'][20]
+img5_0_idx = digits_idc['5'][0]
+img5_1_idx = digits_idc['5'][20]
+img6_0_idx = digits_idc['6'][0]
+img6_1_idx = digits_idc['6'][20]
+img9_0_idx = digits_idc['9'][0]
+img9_1_idx = digits_idc['9'][20]
+
+encoding_2_0 = normalized_encodings[img2_0_idx]
+encoding_2_1 = normalized_encodings[img2_1_idx]
+
+encoding_5_0 = normalized_encodings[img5_0_idx]
+encoding_5_1 = normalized_encodings[img5_1_idx]
+
+encoding_6_0 = normalized_encodings[img6_0_idx]
+encoding_6_1 = normalized_encodings[img6_1_idx]
+
+encoding_9_0 = normalized_encodings[img9_0_idx]
+encoding_9_1 = normalized_encodings[img9_1_idx]
+
+# Visualization
+plt.figure(1)
+pixels = np.array(x_test[img2_0_idx], dtype='uint8')
+pixels = pixels.reshape((28, 28))
+plt.subplot(421)
+plt.imshow(pixels, cmap='gray')
+
+pixels = np.array(x_test[img2_1_idx], dtype='uint8')
+pixels = pixels.reshape((28, 28))
+plt.subplot(422)
+plt.imshow(pixels, cmap='gray')
+
+pixels = np.array(x_test[img5_0_idx], dtype='uint8')
+pixels = pixels.reshape((28, 28))
+plt.subplot(423)
+plt.imshow(pixels, cmap='gray')
+
+pixels = np.array(x_test[img5_1_idx], dtype='uint8')
+pixels = pixels.reshape((28, 28))
+plt.subplot(424)
+plt.imshow(pixels, cmap='gray')
+
+pixels = np.array(x_test[img6_0_idx], dtype='uint8')
+pixels = pixels.reshape((28, 28))
+plt.subplot(425)
+plt.imshow(pixels, cmap='gray')
+
+pixels = np.array(x_test[img6_1_idx], dtype='uint8')
+pixels = pixels.reshape((28, 28))
+plt.subplot(426)
+plt.imshow(pixels, cmap='gray')
+
+pixels = np.array(x_test[img9_0_idx], dtype='uint8')
+pixels = pixels.reshape((28, 28))
+plt.subplot(427)
+plt.imshow(pixels, cmap='gray')
+
+pixels = np.array(x_test[img9_1_idx], dtype='uint8')
+pixels = pixels.reshape((28, 28))
+plt.subplot(428)
+plt.imshow(pixels, cmap='gray')
+plt.show()
 
 print('2 & 2: {}'.format(tf.norm(encoding_2_0 - encoding_2_1).numpy()))
 print('2 & 5: {}'.format(tf.norm(encoding_2_0 - encoding_5_0).numpy()))
@@ -75,3 +161,4 @@ print('5 & 6: {}'.format(tf.norm(encoding_5_0 - encoding_6_0).numpy()))
 print('9 & 9: {}'.format(tf.norm(encoding_9_0 - encoding_9_1).numpy()))
 print('5 & 9: {}'.format(tf.norm(encoding_5_0 - encoding_9_0).numpy()))
 print('6 & 9: {}'.format(tf.norm(encoding_6_0 - encoding_9_0).numpy()))
+
