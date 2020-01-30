526058f7794229e4ef4e2055bf22dffeb0031eb3
Larry Liang <lian999111@gmail.com>
2020-01-30 22:11:49 +0100

    Intensify the images

--- test_center_loss.py
+++ test_center_loss.py
@@ -15,15 +15,16 @@ model = k.models.load_model('log/centerloss/model_v4_no_5.h5')
 # %% Load images
 test_imgs = []
 for im_path in glob.glob("test_imgs/*.jpg"):
-     img = -imageio.imread(im_path)
-     plt.imshow(img, cmap='gray')
-     plt.show()
+     img = 255-imageio.imread(im_path)  # invert the image
+     # plt.imshow(img, cmap='gray')
+     # plt.show()
      test_imgs.append(img)
 
-test_imgs = np.array(test_imgs)
+test_imgs = np.array(test_imgs, dtype=np.float)
 test_imgs = np.expand_dims(test_imgs, axis=-1)
+test_imgs = np.minimum(test_imgs*1.5, 255.0)
 test_imgs = test_imgs / 255.0
-# test_imgs = 1 - test_imgs     # invert the image
+
 
 # %% Compare test images
 encodings = model.predict(test_imgs)
